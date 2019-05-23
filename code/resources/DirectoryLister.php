25d9e43e64492f6e97f48874a13503dbfd1dde91
Lucas Avila <avila.lucas@colonseguros.com.ar>
2019-05-23 15:13:54 -0300

    se hacen modificaciones

--- code/resources/DirectoryLister.php
+++ code/resources/DirectoryLister.php
@@ -828,7 +828,7 @@ class DirectoryLister {
         }
 
         // Build the application URL
-        $appUrl = $protocol . $host . $path;
+        $appUrl = $protocol . $host . $path . '/automation-report/' ;
 
         // Return the URL
         return $appUrl;
