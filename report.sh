1448d1ee299ff065b8adf06ded468b5dd92f80ed
Lucas Avila <avila.lucas@colonseguros.com.ar>
2019-06-06 12:36:00 -0300

    se pisa plataforma canal

--- report.sh
+++ report.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+ENVIRONMENT_PARAM=$1
 fecha=`date +"%d-%m-%Y"`
 hora=`date +"%H-%M"`
 
 cd /webapps/redbee/folder-explorer/code/
 
-mkdir -p "$fecha"/"$hora"
+mkdir -p "$ENVIRONMENT_PARAM"/"$fecha"/"$hora"
 
-sudo cp -rf /webapps/redbee/jenkins/jenkins_home/workspace/AUTOMATION-WEB/TestErrores/* /webapps/redbee/folder-explorer/code/$fecha/$hora
+sudo cp -rf /webapps/redbee/jenkins/jenkins_home/workspace/AUTOMATION-WEB/TestErrores/* /webapps/redbee/folder-explorer/code/$ENVIRONMENT_PARAM/$fecha/$hora
 
